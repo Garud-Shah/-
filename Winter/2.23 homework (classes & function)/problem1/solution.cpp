415585fe1c6c02becb46386f1a66de61841802b0
Garud Shah <garudshah1@gmail.com>
2024-02-29 18:58:47 -0500

    .

---
+++ Winter/2.23 homework (classes & function)/problem1/solution.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct student {
+    int studentID;
+    string Name;
+    int grade;
+};
+int main(){
+    vector<student> students = {};
+
+}
