bfb0909e3475e636f20937e6dacf2fcfc01416e3
Garud Shah <garudshah1@gmail.com>
2024-03-11 21:16:40 -0700

    .

---
+++ Winter/std::sort and std::set are better anway/3.5 homework/Question 2/selection_sorting_stable.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void selectionsort(vector<pair<int,int>> &list){
+    int n = list.end() - list.begin();
+    for (int i = 0; i < (n-1); i++){
+        int pos = i;
+        for (int j = i + 1; j < n; j++){
+            if (list[pos].first >= list[j].first){
+                pos = j;
+            }
+        }
+        pair<int,int> temp = list[i];
+        list[i] = list[pos];
+        list[pos] = temp;
+    }
+}
+void printoutlist(vector<pair<int,int>> list){
+    cout << "The list is: " << endl;
+    for (auto i: list){
+        cout << i.first << " " << i.second << endl;
+    }
+}
+int main(){
+    vector<pair<int,int>> list = {{3,1},{3,2},{6,1},{2,1},{3,3},{7,1},{4,1},{3,4}};
+    selectionsort(list);
+    printoutlist(list);
+}
