083a51b6a82f8e2c278c9a2d2324fbd838984cfd
Garud Shah <garudshah1@gmail.com>
2024-04-08 20:17:06 -0700

    .

--- Spring/sorting? libaries, std::set, std::map.../4.2 class/bubblesort.cpp
+++ Spring/sorting? libaries, std::set, std::map.../4.2 class/bubblesort.cpp
@@ -1,13 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <cstdlib> 
+#include <time.h> 
 using namespace std;
-bool is_sorted(vector<int> list){
-    
-    return true;
+void bubble_sort(vector<int> &list){
+    for (int i=0; i<(list.size()-1); i++){
+        bool is_sorted = true;
+        for (int j=0; j < (list.size() - i - 1); j++){
+            if (list[j] > list[j+1]){
+                int temp = list[j];
+                list[j] = list[j+1];
+                list[j+1] = temp;
+                is_sorted = false;
+            }
+        }
+        if (is_sorted == true){
+        }
+    }
 }
-void bubblesort(){
-    
+void print_list(vector<int> list){
+    cout << "list is: ";
+    for (auto i: list){
+        cout << i << " ";
+    }
+    cout << endl;
 }
 int main(){
-
+    srand(time(0)); 
+    vector<int> list = {};
+    for (int i=0; i<(rand() % 100); i++){
+        list.push_back(rand() % 100);
+    }
+    cout << "Unsorted "; print_list(list);
+    bubble_sort(list);
+    cout << "Sorted "; print_list(list);
 }
--- Spring/sorting? libaries, std::set, std::map.../4.2 class/bubblesort.py
+++ Spring/sorting? libaries, std::set, std::map.../4.2 class/bubblesort.py
@@ -10,7 +10,13 @@ def bubblesort(alist):
                 temp = mylist[j]
                 mylist[j] = mylist[j+1]
                 mylist[j+1] = temp
+        if (issorted(mylist)):
+            return mylist
     return mylist
-         
-
-print("List is: ")
+def issorted(mylist):
+    for i in range(len(mylist)-1):
+        if (mylist[i] > mylist[i+1]):
+            return False
+    return True
+print("List is: ", mylist)
+print("Sorted list is: ", mylist)
